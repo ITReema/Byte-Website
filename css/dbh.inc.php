3a87b5135d67cb15193b061050f5e576a8578efb
ITReema <it.reema7@gmail.com>
2019-07-16 14:26:53 +0300

    init commit

---
+++ css/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn=mysqli_connect('localhost','root','','commentsection2');
+   
+   if(!$conn)
+   die("connection faild: ".mysql_connect_error());
+
+?>
---
+++ home.php
@@ -0,0 +1,219 @@
+<?php
+	
+	date_default_timezone_set('Asia/Riyadh');
+	include 'comments.inc';
+	include 'dbh.inc.php.txt';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <title>  </title>
+         <meta charset="utf-8">
+         <meta name ="author " content="  ">
+         <meta name ="description" content ="">
+         <meta http-equiv = "X-UA-Compatible" content="IE-edge">
+         <meta name ="viewport" content ="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    </head>
+    
+     
+     <body>
+    <div id="container">
+      <!-- Header -->  
+       	<header id="header">
+           
+            
+            
+           
+             <nav id="menu">
+    <h1 id ="logo"> بايت  </h1>
+  <ul class="menu-all">
+  
+     <li class="menu-itme"><a href="#end"> انجازتنا </a></li>
+   <li class="menu-itme"><a href="#view">  رؤيتنا </a> </li>
+     <li class="menu-itme"><a href="#service"> خدماتنا</a></li>
+     <li class="menu-itme"><a href="#our">من نحن  </a></li>
+    </ul>
+  </nav>
+       </header>
+  
+      <!-- hero img -->
+          
+             <div id="hero">
+           <img src="https://gm1.ggpht.com/bNZsN6xQ3rHBcCvzwUGM51jFqGXQfE3-kB6leSy9VBgxB5Zg7et6hyl30jFBfw0ef9GJJ2-jxlN2DS6d083oGxOBMOy3k2X2McDXJXPVNb6sQDylAi2raPPLaLjJM_mplApyQvWaRxJo03SMpCYleBWpdeZDlQmIo2P90j7eQUbfTlwCkz1brUuD8NIcSVbmKEwCHTXqV9mVDdlK_ADRMVHgGjGAANTj7wYgJcugb1PtfNJGA8JkAqVRo8460tdPUjDrMFNN7IgTfeqUnbk5u5zESihazu3KzZmNFEOMRHyXZnJadA9v43ycm7lM17MJe1rBoVsbxhwBwXB0Xr-reT9gB_Tv33GXBKDAHUGNa1-1j9Mdr88t92y5BMKRj_oZHqZd4CAoqQBXS6ABho96P5HePQnu1Bzz4cSbaNGALdBovwcFYpJeraD6FPjT0fL_3_Mdz_vKc_mgpGwOG1-t_z6iq_9O5aEO9btIlTqMM67qsC9lYIQQ_0d7K8B9BppRE9d8LKrdxaIeXUI6NqpO8PXp2SM1ljENmNXVUnX4hmNz3jLJcvvSgqATeaydr-25T5FbXahVH3BpFiXJiGI_9DS7g6beF8qBhhQhCC2OQ0OHLtC-9IHE1IbRwYhiz7pAzxWKPY2-7-VDzeLbUaIwihfG_z1jW8oXsdhD2ZgfiBobOboBn_e2ClwNgKDEz5PPcanDnmjUM8w2F4ijCiaVqkw-YxQt5RS2qpj2=s0-l75-ft-l75-ft" width=100% height=600px alt="picture">
+               
+
+           
+       </div> 
+
+   
+			<section id="colum">
+               
+                    <header  class="about"><br>
+					<p> <h5 id = "our" ></h5>❀❀❀</p><br>
+						<h2  class="hh"> ◈◇ بايت ◇◈ </h2>
+						
+				
+				
+				
+                        <p> <h5 > نحن كيان متكامل نعتني بك من بداية وضع حجر الأساس إلى نهاية مشوارك.
+
+نحن نقدم مجموعة كاملة من الخدمات التقنية سهلة الاستخدام مثل تصميم وبرمجة مواقع الإنترنت، إستضافة موثوق بها، تطبيقات الجوال، نظام تهيئة المواقع لملائمة محركات البحث (SEO)، تصميم شعارات متخصصة ومتميزة، عروض برمجياتت, وغيرها من الخدمات.</p>
+                        </h5><br>
+				
+				<p> <h5 id = "service" ></h5>❀❀❀</p>
+				
+					<br>
+                       <h2  class="hh">:يسعدنا أن نقدم لكم الخدمات في هذه المنصات   </h2><br>
+					</header>
+          
+						<section><br><br><br><br>
+						<br>
+						<br>
+						<br>
+						<br>
+							<div class="col-3 left">
+								<i class="fa fa-globe" style="font-size:90px;color:#A2D9CE"></i>
+									<h3>تطوير مواقع ويب </h3>
+							</div>
+						</section>
+                      
+						<section>
+							<div class="col-3">
+								<i class="fa fa-android" style="font-size:90px;color:#A2D9CE"></i>
+									  <h3 >تطبيقات اندرويد </h3>
+							    </div>
+						</section>
+                  
+						<section>
+							<div class="col-3">
+                           	<i class="fa fa-apple" style="font-size:90px;color:#A2D9CE"></i> 
+									<h3>تطبيقات ابل  </h3>
+							</div>
+						
+						</section> </section> 
+      
+			<section>
+				<div class="aa">
+				
+				<p> <h5 id = "view" ></h5>❀❀❀</p><br>
+					<h2  class="hh">رؤيتنا </h2>
+                    <div class="pr">
+					<p  >نقدر علاقاتنا مع عملاءنا الذين يعملون معنا ونطبق أفضل الأساليب الإدارية من خلال تطوير المهارات ورفع مستوى الكفاءات والارتقاء بالمعرفة وتقديم الحلول التكنولوجية حتى نظل شركة موثوقاً بها في المجال. كما نحلل الاتجاهات التجارية بواسطة حلول تكنولوجية متطورة تناسب التحديات المستجدة في السوق </p  >
+                    </div>
+                    </div>
+                  </section>      
+   <style>
+.aa {
+    background-image: url(https://cdn.pixabay.com/photo/2017/05/19/06/22/desktop-2325627_1280.jpg); }
+       
+</style>
+ 
+  <div id= "content">
+  
+ 
+  <div class="coment-2">
+ 
+  <?php
+	echo
+	"<form method='POST' action='#".setcomment($conn)." '>
+	
+	<input type='hidden' name ='date' value=' " .date('Y-m-d H:i:s'). " '>
+	
+	
+	<h5><label>:الاسم<br>
+	<input name = 'NAME' type = 'text' size = '25' required=''>
+	</label></h5>
+	<h5><label> ! رأيك يهمنا  </h5>
+	<textarea name = 'MESSAGE'
+	rows = '6' cols = '80' > </textarea>
+	</label><br>
+	
+	<input class = 'button' type='submit' name='commentSubmit' value ='Comment'>
+	<input class = 'button' type = 'reset' value = 'clear'>
+	</p>
+	<br>
+	
+	</form> ";
+		
+		getcomment($conn); "<br> <br>"
+		?>
+		<br>
+		</div>
+  </div>
+
+
+			<br><section ><br><hr>
+                <div id="linkA"  >
+                <header id="linkH" >
+						<h2 id = "end" class="m">أحدث أعمالنا   </h2>
+					
+					</header>
+              
+				
+                    <div class="lin">
+						<section>
+						<div class="link l1 ">	
+				   <img src="imgs/tw.png" width="50px alt="alt="picture ">
+                            
+							</div>		
+				
+						</section>
+                 
+						<section>
+							<div class="link l2">
+						<img src="imgs/we.png" width="130px" alt="picture ">
+                                
+									  
+							  </div>
+						</section>
+                      
+						<section>
+                            <div class="link l3">
+							<img src="imgs/s.png" width="80px" alt="picture ">
+                                
+							</div>
+						</section>
+				    </div>
+                    </div>	
+		       </section>
+   
+
+			<footer id="footer">
+				
+					<div class="Communiction">
+					   
+							
+				   <i class="fa fa-twitter-square" style="font-size:40px;color:#f3f3f3"></i>
+                   <i class="fa fa-instagram"id="in"style="font-size:40px;color:#f3f3f3 "></i>	
+						
+					</div>
+					<div class="emal">
+						<p>جميع الحقوق محفوظه لصاحب الموقع </p>
+                        <p><a href="mailto:bit883.gmail.com">emal:bit883.gmail.com</a></p>
+					</div>
+				
+       </footer>
+   
+       </div>
+  <!-- script-->  
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
+    <script src="js/script.js"></script>
+    </body>
+
+
+
+
+
+
+
+
+
+</html>
+
+
